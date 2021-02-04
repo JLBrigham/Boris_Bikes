a9e0f900b4e8c98433282b124a282c7a485ba333
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-02-04 15:49:39 +0100

    Step 9 Build a bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,12 +1,11 @@
 require 'Docking_station'
 
-describe 'DockingStation' do
+describe DockingStation do
   it 'check if Dockingstation exists' do
     expect(DockingStation.new.is_a?(DockingStation)).to eq true
   end
 
-  #it {to respond_to(release_bike)}
-  it 'check if responds to release_bike' do
-    expect(DockingStation.new.respond_to?(:release_bike))
+  it 'should respond to release_bike method' do
+    expect(subject).to respond_to(:release_bike)
   end
-end
+  end
